418f0ec73fa659f0e40f6848bbb78333e9fe0970
Sima <sima.boreiri@gmail.com>
2020-07-21 10:52:41 +0200

    modification

--- src/component/Movie.jsx
+++ src/component/Movie.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
-import { Button, Segment, Message, Header, Menu, Grid, GridColumn } from 'semantic-ui-react'
+import { Button, Segment, Message  } from 'semantic-ui-react'
 
 class Movie extends Component {
   state = {
